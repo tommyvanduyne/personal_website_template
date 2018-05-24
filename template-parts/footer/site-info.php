7ff3edaaedf8e529da73effbd83cf8896d383201
Tommy Van Duyne <tommyvanduyne@gmail.com>
2018-05-24 19:09:34 +0000

    updated the wordpress site to be a child theme of the wordpress twentyseventeen theme

---
+++ template-parts/footer/site-info.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Displays footer site info
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.0
+ */
+
+?>
+<div class="site-info">
+	<?php
+	if ( function_exists( 'the_privacy_policy_link' ) ) {
+		the_privacy_policy_link( '', '<span role="separator" aria-hidden="true"></span>' );
+	}
+	?>
+	<a href="<?php echo esc_url('http://tomvanduyne.com'); ?>" class="imprint">
+		<?php echo 'Tommy Van Duyne &copy;	2018'; ?>
+	</a>
+</div><!-- .site-info -->
