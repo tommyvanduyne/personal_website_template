75011d7df3a51e60674a30e5cf4d0b3f0161eb8e
Tommy Van Duyne <tommyvanduyne@gmail.com>
2018-06-06 20:35:41 +0000

    commiting to make some changes

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 //get parent styles
-add_action( 'wp_enqueue_scripts', 'enqueue_child_theme_styles', PHP_INT_MAX);
+add_action( 'wp_enqueue_scripts', 'enqueue_child_theme_styles');
 function enqueue_child_theme_styles() {
 	  wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
 }
